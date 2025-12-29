a3f5c866221fd54f515b836892d9869b30d43391
Keks <keks@htmlacademy.ru>
2025-12-29 14:26:14 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,16 @@
+import {createPhotosArray} from './data.js';
+import { renderThumbnails } from './thumbnails.js';
+
+// Создаем временные данные для разработки
+const photos = createPhotosArray();
+
+// Отрисовываем миниатюры
+renderThumbnails(photos);
+
+// Для тестирования - выводим информацию в консоль
+// eslint-disable-next-line no-console
+console.log(`Отображено ${photos.length} фотографий`);
+// eslint-disable-next-line no-console
+console.log(
+  createPhotosArray()
+);
